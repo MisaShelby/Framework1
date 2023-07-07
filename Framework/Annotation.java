28d2d90d365e99b75c3fa75c54396ba317b8f1d9
MioraPriscaa <120918876+MisaShelby@users.noreply.github.com>
2023-07-07 22:28:05 +0200

    4

--- Framework/Annotation.java
+++
@@ -1,9 +0,0 @@
-package etu1759.framework.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface Annotation {
-    public String Url() default "";
-}
