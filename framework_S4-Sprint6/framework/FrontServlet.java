e9ffa54d8a8627ef09f438bce266c6cef4e2dfd6
MioraPriscaa <120918876+MisaShelby@users.noreply.github.com>
2023-07-07 22:38:18 +0200

    6

---
+++ framework_S4-Sprint6/framework/FrontServlet.java
@@ -0,0 +1,69 @@
+package etu1759.framework.servlet;
+
+import etu1759.framework.Mapping;
+import etu1759.framework.utils.Fonctions;
+import etu1759.annotation.url;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+
+import java.io.*;
+import java.util.*;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class FrontServlet extends HttpServlet{
+
+    HashMap<String, Mapping> mappingUrls;
+
+    public void init() throws ServletException {
+        
+        try {
+            String packageName = getServletContext().getInitParameter("packageName");
+// sprint3
+            mappingUrls = Fonctions.mameno_HashMap(mappingUrls, packageName);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+// sprint5
+        ModelView modelView = Fonctions.recup_ModelView(mappingUrls, request, response);
+// sprint6
+        Fonctions.recuperationData(modelView, request, response);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/"+modelView.getUrl());
+        requestDispatcher.forward(request, response);
+        
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        String packageName = getServletContext().getInitParameter("packageName");
+        URL root = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "//")); 
+        out.println("Front Servlet");   
+        out.println("Root: " + root);
+         for(String key : mappingUrls.keySet()){
+            Mapping mapping = mappingUrls.get(key);
+            out.println("Cle: " + key + ", ClassName: "+ mapping.getClassName() + ", Mapping: " + mapping.getMethod());
+            out.println("<br>"); 
+        }
+    }
+
+    // protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    //     processRequest(request, response);
+
+    // }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+        
+        
+    }
+
+
+}
+
---
+++ framework_S4-Sprint6/test-framework/WEB-INF/classes/Emp.java
@@ -0,0 +1,28 @@
+package models;
+
+import etu1759.annotation.url;
+import etu1759.framework.servlet.ModelView;
+
+public class Emp{
+    String nom;
+    String prenom;
+
+// sprint5: maka modelView anaovana dispatcher
+    @url(value="emp-nomComplet")
+    public ModelView getNomComplet(){
+        ModelView modelView = new ModelView();
+        modelView.setUrl("nomComplet.jsp");
+        return modelView;
+    }
+
+// sprint6: maka donnée anle modelView ho dispatchena
+    @url(value="emp-recupDonnee")
+    public ModelView getDonnees(){
+        ModelView modelView = new ModelView();
+        modelView.setUrl("recupDonnee.jsp");
+        modelView.addItem("nom","Rakotondrasoa");
+        modelView.addItem("prenom","Zety");
+        String nomComplet = this.nom +" "+ this.prenom;
+        return modelView;
+    }
+}
